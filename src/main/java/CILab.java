bde0e9f53c6d2802f4db05ef9b9ae032a029e77c
Aden Shukuroff <aden.shukuroff@bellevuecollege.edu>
2021-06-15 23:21:35 -0700

    implemented detectCapitalUse

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,7 +13,27 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
+        if (myString.isEmpty()) {
+            return true;
+        }
+        for (var word : myString.split("\\s+")) {
+            if (!(word.codePoints().allMatch(Character::isUpperCase) || word.codePoints().allMatch(
+                    Character::isLowerCase))) {
+                if (word.length() == 0) {
+                    continue;
+                }
+                var characters = word.codePoints().toArray();
+                if (!Character.isUpperCase(characters[0])) {
+                    return false;
+                }
+                for (int i = 1; i < characters.length; ++i) {
+                    if (Character.isUpperCase(characters[i])) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
 }
